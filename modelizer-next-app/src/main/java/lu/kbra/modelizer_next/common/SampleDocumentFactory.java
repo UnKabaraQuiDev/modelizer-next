3f81c1afe271b2f51ae1c1868170c5e3bd275110
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-05-07 09:58:51 +0200

    Tried fixing SampleDocumentFactory

--- modelizer-next-app/src/main/java/lu/kbra/modelizer_next/common/SampleDocumentFactory.java
+++ modelizer-next-app/src/main/java/lu/kbra/modelizer_next/common/SampleDocumentFactory.java
@@ -2,6 +2,7 @@ package lu.kbra.modelizer_next.common;
 
 import java.io.IOException;
 
+import lu.kbra.modelizer_next.MNMain;
 import lu.kbra.modelizer_next.document.ModelDocument;
 import lu.kbra.modelizer_next.json.ModernModelizerImporter;
 import lu.kbra.pclib.PCUtils;
@@ -12,7 +13,7 @@ public final class SampleDocumentFactory {
 
 	public static ModelDocument create() {
 		try {
-			return ModernModelizerImporter.importString(PCUtils.readPackagedStringFile(SampleDocumentFactory.class, "/sample.mn"));
+			return ModernModelizerImporter.importString(PCUtils.readPackagedStringFile(MNMain.class, "/sample.mn"));
 		} catch (final IOException e) {
 			return null;
 		}
